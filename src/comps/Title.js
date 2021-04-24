ace484179ea7f1caf440d0210a30ccf728752f35
imtesaalahmed <imtesaalahmed7@gmail.com>
2021-04-24 20:18:25 +0530

    Initial commit

---
+++ src/comps/Title.js
@@ -0,0 +1,14 @@
+  
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>PiXtagram</h1>
+      <h2>Share Your Moments</h2>
+      <p>Your photo gallery :D </p>
+    </div>
+  )
+}
+
+export default Title;
